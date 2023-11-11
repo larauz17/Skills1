d98c37387f270323df7b4ed2a907a8b8781e9408
larauz17 <snctip@gmail.com>
2023-11-11 17:01:51 +0100

    Emmeset per examen

---
+++ src/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/** 
+ * Fitxer de configuració de l'aplicació.
+ * */ 
+
+$config = [
+    "db" => [
+        "user" => "root",
+        "pass" => "",
+        "db" => "apt-fig",
+        "host" => "localhost"
+    ],
+];
